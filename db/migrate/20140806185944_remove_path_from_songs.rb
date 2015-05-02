13b96180ad8a5b9f7e3a43bbb6a224ba00013330
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-05-02 03:33:52 +0200

    fix migrations
    make migrations use raw sql so they are not dependent on ActiveModel.
    Using models inside migrations will fail if the model expects a table
    layout that doesn’t exist yet.

--- db/migrate/20140806185944_remove_path_from_songs.rb
+++ db/migrate/20140806185944_remove_path_from_songs.rb
@@ -1,36 +1,40 @@
 class RemovePathFromSongs < ActiveRecord::Migration
-  def change
-	Song.all.each do |s|
-		begin
-			next if s.path.to_s.empty?
-			if s.path.start_with? 'stoffi:track:youtube:'
-				id = s.path[21..-1]
-				name = 'youtube'
+	def change
+		sql = "select id,path,foreign_url from songs"
+		rows = ActiveRecord::Base.connection.execute sql
+		rows.each do |row|
+			begin
+				path = row[1]
+				next if path.blank?
+				if path.start_with? 'stoffi:track:youtube:'
+					id = path[21..-1]
+					name = 'youtube'
 				
-			elsif s.path.start_with? 'stoffi:track:soundcloud:'
-				id = s.path[24..-1]
-				name = 'soundcloud'
+				elsif path.start_with? 'stoffi:track:soundcloud:'
+					id = path[24..-1]
+					name = 'soundcloud'
 				
-			elsif s.path.start_with? 'stoffi:track:jamendo:'
-				id = s.path[21..-1]
-				name = 'jamendo'
+				elsif path.start_with? 'stoffi:track:jamendo:'
+					id = path[21..-1]
+					name = 'jamendo'
 				
-			else
-				id = s.path
-				name = 'local'
+				else
+					id = path
+					name = 'local'
+				end
+			
+				src = Source.new
+				src.resource_tyoe = 'Song'
+				src.resource_id = row[0]
+				src.name = name
+				src.foreign_id = id
+				src.foreign_url = row[2]
+			
+			rescue
 			end
 			
-			src = Source.new
-			src.resource = s
-			src.name = name
-			src.foreign_id = id
-			src.foreign_url = s.foreign_url
-			
-		rescue
 		end
-			
+		remove_column :songs, :path, :string
+		remove_column :songs, :foreign_url, :string
 	end
-    remove_column :songs, :path, :string
-    remove_column :songs, :path, :string
-  end
 end
