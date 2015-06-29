29e3c7dc1d9045ca735399f7bb6cd8e840bc9a88
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-06-29 11:51:31 +0200

    set force_ssl from env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = ENV.key?('SSL')
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
