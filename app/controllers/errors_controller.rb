7ac145bfa070f513cc578a675834b723fff21d5c
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-07-13 00:06:36 +0200

    Create pretty error pages
    Create a error controller which handles displaying errors in a more
    dynamic, pretty way.

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,21 @@
+class ErrorsController < ApplicationController
+	
+	def show
+		@code = status_code
+		
+		if @code.to_i == 404
+			@resource = "page"
+			render '404', status: :not_found
+			
+		else
+			render 'show', status: @code.to_i, layout: 'empty'
+		end
+	end
+	
+	protected
+	
+	def status_code
+		params[:code] || 500
+	end
+	
+end
